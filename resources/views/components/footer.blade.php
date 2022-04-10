6c45f18c4b32d1e87c254620cb14a9fd63a6a9c1
fillahaufi <fillahaufi140101@gmail.com>
2022-04-10 16:06:08 +0700

    basic views

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,4 @@
+<div class="bg-dark">
+    <p class="m-0 text-white text-center">Copyright &copy; Made by Fikri Sandi</p>
+
+</div>
---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,46 @@
+<div uk-sticky="sel-target: .uk-navbar-container; cls-active: uk-navbar-sticky; bottom: #transparent-sticky-navbar">
+    <nav class="uk-navbar-container-fluid px-5 bg-dark" uk-navbar style="position: relative; z-index: 980;">
+        <div class="uk-navbar-left">
+
+            <ul class="uk-navbar-nav">
+                <li class="uk-active">
+                    <a href="{{ url('/') }}">
+                        <h3 class="mb-0 fw-bold text-white">
+                            Inventory-App
+                        </h3>
+                    </a>
+                </li>
+                {{-- Auth user --}}
+                <li class="ms-3">
+                    <a href="#">Ajukan Peminjaman</a>
+                    <div class="uk-navbar-dropdown bg-dark">
+                        <ul class="uk-nav uk-navbar-dropdown-nav">
+                            <li><a href="{{ url('/barang') }}" class="fw-regular">Barang</a></li>
+                            <li><a href="#" class="fw-regular">Ruangan</a></li>
+                        </ul>
+                    </div>
+                </li>
+
+            </ul>
+
+        </div>
+        <div class="uk-navbar-right">
+            {{-- guest user --}}
+            <ul class="uk-navbar-nav">
+                <li class="mx-2" style="align-self: center">
+                    <button class="uk-button uk-button-secondary bg-white text-dark">Login</button>
+                </li>
+                <li class="ms-2" style="align-self: center">
+                    <button class="uk-button uk-button-default text-white">Register</button>
+                </li>
+            </ul>
+
+            {{-- Auth user --}}
+            <ul class="uk-navbar-nav">
+                <li class="ms-2" style="align-self: center">
+                    <button class="uk-button uk-button-default text-white">Logout</button>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</div>
